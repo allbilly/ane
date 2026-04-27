bf588f94126ac658a1467aa11de0f98794ee2b46
allbilly <53866088+allbilly@users.noreply.github.com>
2026-04-27 23:25:39 +0800

    run.py: support single-input models (conv, gemm, relu, sigmoid)
    Check m.src_count before accessing src_nchw[1] to handle
    models with only 1 input.

--- run.py
+++ run.py
@@ -1,11 +1,12 @@
 import sys, numpy as np
 from ane import model  
 
-model = model(sys.argv[1])
-# Only need (1, 64, 1, 1) from model.src_nchw (1, 64, 1, 1, 64, 64) 
-a = np.full(model.src_nchw[0][:4], 3, dtype=np.float16)
-b = np.full(model.src_nchw[1][:4], 2, dtype=np.float16)
-
-out = model.predict([a, b])[0]
+m = model(sys.argv[1])
+a = np.full(m.src_nchw[0][:4], 3, dtype=np.float16)
+if m.src_count > 1:
+    b = np.full(m.src_nchw[1][:4], 2, dtype=np.float16)
+    out = m.predict([a, b])[0]
+else:
+    out = m.predict([a])[0]
 print(out.shape, out.dtype)
 print(out[0][0])
