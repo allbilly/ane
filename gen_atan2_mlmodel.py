29493d00db7dd622c887d9adb9f6b1f2b75f480c
allbilly <53866088+allbilly@users.noreply.github.com>
2026-04-29 19:56:21 +0800

    add GH action to generate atan2 and sqrt HWX models

---
+++ gen_atan2_mlmodel.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import coremltools as ct
+
+class Atan2(nn.Module):
+    def __init__(self):
+        super(Atan2, self).__init__()
+    def forward(self, x, y):
+        x = torch.atan2(x, y)
+        return x
+
+model = Atan2().eval()
+
+input = [torch.rand(1024, 2048), torch.rand(1024, 2048)]
+trace = torch.jit.trace(model, input)
+mlmodel = ct.convert(trace, inputs=[ct.TensorType(name="x", shape=input[0].shape),
+                                   ct.TensorType(name="y", shape=input[1].shape)])
+mlmodel.save('atan2.mlmodel')
---
+++ gen_sqrt_mlmodel.py
@@ -0,0 +1,10 @@
+import coremltools as ct
+from coremltools.converters.mil import Builder as mb
+
+@mb.program(input_specs=[mb.TensorSpec(shape=(1024, 2048))])
+def sqrt(x):
+    x = mb.sqrt(x=x)
+    return x
+
+mlmodel = ct.convert(sqrt)
+mlmodel.save('sqrt.mlmodel')
